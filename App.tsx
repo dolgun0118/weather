7a9974fe5261b7214c9fe18a9463d4fa46b8c01d
dolgun0118 <zipo0309@gmail.com>
2023-08-11 22:04:31 +0900

    [New Feature]: Save Temp

--- App.tsx
+++ App.tsx
@@ -1,20 +1,150 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
+import { IconProps } from "@expo/vector-icons/build/createIconSet";
+import iconSet from "@expo/vector-icons/build/Fontisto";
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const API_KEY = "44ddc4720ff049e7e93470bf9d1d1735";
+const ICONS: { [key: string]: any } = {
+  Clear: "day-sunny",
+  Clouds: "cloudy",
+  Rain: "rain",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Drizzle: "day-rain",
+  Thunderstorm: "lightning",
+};
 
-export default function App() {
+const App = () => {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      {
+        useGoogleMaps: false,
+      }
+    );
+    setCity(location[0].city || location[0].region || "null");
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {!days.length && (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        )}
+        {!!days.length &&
+          days.map((day: any, index) => (
+            <View key={index} style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={ICONS[day.weather[0].main]}
+                  color="white"
+                  size={68}
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))}
+      </ScrollView>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 60,
+    fontWeight: "500",
+    color: "white",
+  },
+  weather: {
+    // backgroundColor: "blue",
+  },
+  day: {
+    alignItems: "flex-start",
+    width: SCREEN_WIDTH,
+    paddingHorizontal: 20,
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 100,
+    fontWeight: "600",
+    color: "white",
+  },
+  description: {
+    marginTop: -20,
+    fontSize: 40,
+    color: "white",
+  },
+  tinyText: {
+    marginTop: -10,
+    fontSize: 30,
+    color: "white",
   },
 });
